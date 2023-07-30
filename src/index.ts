16c84f9333eb3fdac573d50af16c7046d57b2d56
Lyudmil Danailov <spiderjako@Lyudmils-MacBook-Pro.local>
2023-07-31 00:19:49 +0300

    Letter Combinations of a Phone Number Problem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('hello world');
+import { letterCombinations } from './solutions/phoneNumber.js';
+
+console.log('Letter combinations:')
+console.log('Input: "23"')
+console.log('Output: [' + letterCombinations('23') + ']');
